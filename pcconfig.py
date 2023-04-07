485496d53a2f0e3f017492206250502e5bf8962a
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-07 10:14:03 +0800

    Update pcconfig.py

--- pcconfig.py
+++ pcconfig.py
@@ -2,6 +2,8 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="Pynecone_DALLE_Render",
+    api_url="http://192.168.1.1:8000",
+    bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
 )
